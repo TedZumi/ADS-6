dec456aeb5ed826677379ef5d14da13db63c8b8a
TedZumi <79110264+TedZumi@users.noreply.github.com>
2021-05-30 20:40:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,25 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::string str = "";
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return tree;
+  }
+  char diff = 'a' - 'A';
+  while (!file.eof()) {
+    char ch = file.get();
+    if (((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z'))) {
+      if ((ch >= 'A') && (ch <= 'Z'))
+        ch += diff;
+      str += ch;
+    } else if (str != "") {
+        tree.add(str);
+        str = "";
+    }
+  }
+  file.close();
+  return tree;
 }
